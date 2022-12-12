2353b9a8ecf37e7bac6d1001798d06f054b64b69
Shamim Sarkar <shamimibas@gmail.com>
2022-12-13 00:24:19 +0600

    Edited footer, Added particle animation, and simple framer motion

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,14 +1,21 @@
 import React from "react";
-import image from '../../.././images/profile.jpg';
+import image from "../../.././images/profile.jpg";
 import Skills from "../Skills/Skills";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
-    <div id="about-me" className="mt-10">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: false }}
+      id="about-me"
+      className="mt-10"
+    >
       <h1 className="text-2xl font-bold text-center text-primary">About Me</h1>
       <div className="lg:flex justify-center items-center">
         <div className="lg:w-1/4 w-72 mx-auto">
-            <img src={image} alt="" className="mask mask-circle mx-auto"/>
+          <img src={image} alt="" className="mask mask-circle mx-auto" />
         </div>
         <div className="lg:w-1/2 w-screen p-4">
           <p>
@@ -20,9 +27,11 @@ const About = () => {
           </p>
         </div>
       </div>
-      <h1 className="text-2xl font-bold mb-5 mt-10 text-center text-primary">Skills</h1>
-      <Skills/>
-    </div>
+      <h1 className="text-2xl font-bold mb-5 mt-10 text-center text-primary">
+        Skills
+      </h1>
+      <Skills />
+    </motion.div>
   );
 };
 
--- src/Pages/Home/Hero/Hero.js
+++ src/Pages/Home/Hero/Hero.js
@@ -1,44 +1,131 @@
-import React from "react";
-import image from '../../../images/profile.jpg';
-import Typewriter from 'typewriter-effect';
-
-const name = document.getElementById('name');
-const typeWriter = new Typewriter(name, {
-  loop: true,
-  delay: 75,
-})
-
+import React, { useCallback } from "react";
+import image from "../../../images/BG.png";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import "./Hero.css";
+import { motion } from "framer-motion";
 
 const Hero = () => {
+  const particlesInit = useCallback(async (engine) => {
+    console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+  }, []);
+
   return (
-    <div>
-      <div className="hero min-h-screen">
+    <motion.div
+      animate={{ y: 20 }}
+      transition={{
+        delay: 0.5,
+        x: { duration: 2 },
+        default: { ease: "linear" }
+      }}
+    >
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          width: "100",
+          fpsLimit: 100,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 150,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#000dff",
+            },
+            links: {
+              color: "#000dff",
+              distance: 180,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              directions: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 900,
+              },
+              value: 100,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <div id="tsparticles" className="hero min-h-screen">
         <div className="hero-content flex-col justify-between lg:flex-row-reverse">
-          <img
-            src={image}
-            className="max-w-sm lg:w-1/2 md:w-72 w-72 rounded-full shadow-2xl"
-            alt=""
-          />
+          <img src={image} className="max-w-sm lg:w-2/3 md:w-72 w-72" alt="" />
           <div className="lg:w-1/2 mx-auto lg:mt-0 md:mt-0 mt-6">
-            <h1 className="lg:text-5xl text-4xl font-bold lg:text-left text-center">HI, I'M <br/>
-            <span id="name" className="text-primary">
-              SHAMIM SARKAR
+            <h1 className="lg:text-5xl text-4xl font-bold lg:text-left text-center">
+              HI, I'M <br />
+              <span id="name" className="text-primary">
+                SHAMIM SARKAR
               </span>
             </h1>
             <p className="py-6 lg:text-left text-center">
-                Full-Stack Web Developer | MERN Stack Developer | REACT Developer
+              Full-Stack Web Developer | MERN Stack Developer | REACT Developer
             </p>
             <p className="lg:text-left text-center">
-            <a href="Shamim-sarkar-resume.pdf"
-            download='Shamim-sarkar-resume.pdf'
-             className="btn btn-primary">
+              <a
+                href="Shamim-sarkar-resume.pdf"
+                download="Shamim-sarkar-resume.pdf"
+                className="btn btn-primary"
+              >
                 DOWNLOAD RESUME
-            </a>
+              </a>
             </p>
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
